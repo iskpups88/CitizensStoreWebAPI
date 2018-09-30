739c65866856c1605350e00280db6f55e633d5d2
Искандер Хакимжанов <ctanin2@gmail.com>
2018-09-30 17:39:05 +0300

    initial

--- ExtJsWebApi/Startup.cs
+++ ExtJsWebApi/Startup.cs
@@ -41,6 +41,8 @@ namespace ExtJsWebApi
             }
 
             app.UseHttpsRedirection();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
